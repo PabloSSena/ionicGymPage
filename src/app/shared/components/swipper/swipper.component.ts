b194aec7269ec3ca31c42a534caaf412e2916dea
Pablo Sena <pablosilvasena@gmail.com>
2024-02-29 23:22:41 -0300

    swipper Feito

--- src/app/shared/components/swipper/swipper.component.ts
+++ src/app/shared/components/swipper/swipper.component.ts
@@ -1,18 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, Input, OnInit } from '@angular/core';
-import { IonicSlides } from '@ionic/angular';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-swipper',
   templateUrl: './swipper.component.html',
   styleUrls: ['./swipper.component.scss'],
   standalone:true,
-  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+  imports:[
+    CommonModule,
+    IonicModule
+  ]
 })
 export class SwipperComponent  implements OnInit {
-  @Input() slides: any[] = []
-  swiperModules = [IonicSlides]
+  content: any[] = []
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.content = [
+      {
+        img:"../../../../assets/fotoAcademia1.jpg",
+        text:"Novo Treino"
+      },
+      {
+        img:"../../../../assets/fotoAcademia2.jpg",
+        text:"Yoga Express"
+      }
+  
+    ]
+  }
+
 
 }
